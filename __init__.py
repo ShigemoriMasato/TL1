b7fd6c21869c54c9154e134a657dbb9dd50731d0
ShigemoriMasato <k024g0027@g.neec.ac.jp>
2026-04-21 12:47:32 +0900

    01-06

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 #ブレンダーに登録するアドオン情報
 bl_info = {
@@ -43,6 +44,40 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
 
         #オペレーターの命令終了を通知
         return {'FINISHED'}
+    
+
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーンを出力します"
+
+    def execute(self, context):
+
+        print("シーン情報をExportします")
+
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)
+            #ローカルトランスフォーム行列から平行移動、回転、スケーリングを抽出
+            #型はVector, Quaternion, Vector
+            trans, rot, scale = object.matrix_local.decompose()
+            #回転をQuaternionからEulerに変換
+            rot = rot.to_euler()
+            #ラジアンから度数法に変換
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+
+            print("Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z))
+            print("Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z))
+            print("Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
+
+            if object.parent:
+                print("Parent:" + object.parent.name)
+            print()
+        print("シーン情報をExportしました")
+        self.report({'INFO'}, "シーン情報をExportしました")
+
+        return {'FINISHED'}
 
 #トップバーの拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types.Menu):
@@ -58,15 +93,15 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         #トップバーの「エディターメニュー」に項目(オペレーター)を追加
         self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
         self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label)
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname, text=MYADDON_OT_export_scene.bl_label)
 
     #既存のメニューにサブメニューを追加
     def submenu(self, context):
         #ID指定でサブメニューを追加
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
-
 #自作クラスまとめ
-classes = (MYADDON_OT_stretch_vertex, MYADDON_OT_create_ico_sphere, TOPBAR_MT_my_menu, )
+classes = (MYADDON_OT_stretch_vertex, MYADDON_OT_create_ico_sphere, MYADDON_OT_export_scene, TOPBAR_MT_my_menu, )
 
 #Menu描画関数
 def draw_menu_manual(self, context):
